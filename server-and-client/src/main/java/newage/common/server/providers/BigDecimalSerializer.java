e7370192e156b20de5bd0c996135568b243a7bb4
mykhailo.gevak <mykhailo.gevak@playtech.com>
2017-02-18 17:10:47 +0200

    -

--- server-and-client/src/main/java/newage/common/server/providers/BigDecimalSerializer.java
+++ server-and-client/src/main/java/newage/common/server/providers/BigDecimalSerializer.java
@@ -7,7 +7,6 @@ import java.text.DecimalFormat;
 import com.google.gson.JsonDeserializationContext;
 import com.google.gson.JsonDeserializer;
 import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
 import com.google.gson.JsonParseException;
 import com.google.gson.JsonPrimitive;
 import com.google.gson.JsonSerializationContext;
